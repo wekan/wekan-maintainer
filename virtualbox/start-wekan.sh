76f12f2c70905f5426d83d500ec51cd01b9769bf
Lauri Ojansivu <x@xet7.org>
2018-09-03 01:36:08 +0300

    Add env variables.

--- virtualbox/start-wekan.sh
+++ virtualbox/start-wekan.sh
@@ -19,12 +19,27 @@
         ## Optional: Integration with Matomo https://matomo.org that is installed to your server
         ## The address of the server where Matomo is hosted:
         # export MATOMO_ADDRESS='https://example.com/matomo'
+        export MATOMO_ADDRESS=''
         ## The value of the site ID given in Matomo server for Wekan
         # export MATOMO_SITE_ID='123456789'
+        export MATOMO_SITE_ID=''
         ## The option do not track which enables users to not be tracked by matomo"
         # export MATOMO_DO_NOT_TRACK='false'
+        export MATOMO_DO_NOT_TRACK='true'
         ## The option that allows matomo to retrieve the username:
         # export MATOMO_WITH_USERNAME='true'
+        export MATOMO_WITH_USERNAME='false'
+        # Enable browser policy and allow one trusted URL that can have iframe that has Wekan embedded inside.
+	# Setting this to false is not recommended, it also disables all other browser policy protections
+	# and allows all iframing etc. See wekan/server/policy.js
+	# Default value: true
+	export BROWSER_POLICY_ENABLED=true
+        # When browser policy is enabled, HTML code at this Trusted URL can have iframe that embeds Wekan inside.
+	# Example: export TRUSTED_URL=http://example.com
+        export TRUSTED_URL=''
+        # What to send to Outgoing Webhook, or leave out. Example, that includes all that are default: cardId,listId,oldListId,boardId,comment,user,card,commentId .
+        # Example: export WEBHOOKS_ATTRIBUTES=cardId,listId,oldListId,boardId,comment,user,card,commentId
+	export WEBHOOKS_ATTRIBUTES=''
 	node main.js & >> ~/repos/wekan.log
 	cd ~/repos
 #done
